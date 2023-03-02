94f677095e7b9e246c6b91f8874728040f758d04
Osama magdy <108794970+OsamaHIma@users.noreply.github.com>
2023-03-03 01:26:36 +0200

    push

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,33 +1,33 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import { fadeIn } from '../utils/motion';
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 const InsightCard = ({ index, title, imgUrl, subtitle }) => (
   <motion.div
-    variants={fadeIn('up', 'spring', index * 0.5, 1)}
-    className='flex md:flex-row flex-col gap-4'
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4"
   >
     <img
       src={imgUrl}
-      alt='plant'
-      className='md:w-[270px] w-full h-[250px] rounded-[32px] object-cover '
+      alt="plant"
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover "
     />
-    <div className='flex justify-between items-center w-full '>
-      <div className='flex-1 md:ml-[62px] flex flex-col max-w-[650px] '>
-        <h4 className='font-normal text-white lg:text-[42px] text-[26px] '>
+    <div className="flex justify-between items-center w-full ">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px] ">
+        <h4 className="font-normal text-white lg:text-[42px] text-[26px] ">
           {title}
         </h4>
-        <p className='text-secondary-white text-[14px] lg:text-[20px] mt-[16px] font-normal'>
+        <p className="text-secondary-white text-[14px] lg:text-[20px] mt-[16px] font-normal">
           {subtitle}
         </p>
       </div>
 
-      <div className='lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full border-[1px] border-white bg-transparent cursor-pointer '>
+      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full border-[1px] border-white bg-transparent cursor-pointer ">
         <img
-          src='arrow.svg'
-          alt='arrow'
-          className='h-[40%] w-[40%] object-contain '
+          src="arrow.svg"
+          alt="arrow"
+          className="h-[40%] w-[40%] object-contain "
         />
       </div>
     </div>
--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,37 +1,37 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import styles from '../styles';
-import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
-import { TypingText, StartSteps, TitleText } from '../components';
-import { startingFeatures } from '../constants';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { TypingText, StartSteps, TitleText } from "../components";
+import { startingFeatures } from "../constants";
 
 const GetStarted = () => (
   <section className={`${styles.paddings} z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
+      initial="hidden"
+      whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
-        variants={planetVariants('left')}
+        variants={planetVariants("left")}
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
-          src='/get-started.png'
-          alt='get started'
-          className='w-[90%] h-[90%] object-contain '
+          src="/get-started.png"
+          alt="get started"
+          className="w-[90%] h-[90%] object-contain "
         />
       </motion.div>
       <motion.div
-        variants={fadeIn('right', 'tween', 0.2, 1)}
-        className='flex-[0.75] flex justify-center flex-col '
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col "
       >
-        <TypingText title='| How Metaversus works' />
+        <TypingText title="| How Metaversus works" />
         <TitleText title={<>Get started with a few clicks</>} />
-        <div className='flex flex-col max-w-[371px] mt-[31px] gap-6 '>
+        <div className="flex flex-col max-w-[371px] mt-[31px] gap-6 ">
           {startingFeatures.map((feature, indx) => (
             <StartSteps key={feature} number={indx + 1} text={feature} />
           ))}
